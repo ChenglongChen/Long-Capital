8710508888a3848f9480104d4a22b75a97397b68
Chenglong Chen <c.chenglong@gmail.com>
2023-02-25 19:37:15 +0800

    adjust hold_thresh=2

--- longcapital/contrib/model/gbdt.py
+++ longcapital/contrib/model/gbdt.py
@@ -1,15 +1,12 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-import numpy as np
-import pandas as pd
+from typing import List, Tuple
+
 import lightgbm as lgb
-from typing import List, Text, Tuple, Union
 from qlib.contrib.model.gbdt import LGBModel as QlibLGBModel
 from qlib.data.dataset import DatasetH
 from qlib.data.dataset.handler import DataHandlerLP
-from qlib.data.dataset.weight import Reweighter
-from qlib.workflow import R
 
 
 class LGBModel(QlibLGBModel):
@@ -26,7 +23,7 @@ class LGBModel(QlibLGBModel):
 
     def _prepare_data(self, dataset: DatasetH, reweighter=None) -> List[Tuple[lgb.Dataset, str]]:
         ds_l = super(LGBModel, self)._prepare_data(dataset=dataset, reweighter=reweighter)
-        for data,key in ds_l:
+        for data, key in ds_l:
             df = dataset.prepare(key, col_set=["feature", "label"], data_key=DataHandlerLP.DK_L)
             group = df.index.get_level_values("datetime").value_counts().sort_index().values
             data.set_group(group)
