8f4d4da92be329fa66ee8937a4b3854ffd99fc61
Chenglong Chen <c.chenglong@gmail.com>
2023-02-25 21:05:25 +0800

    add full topk & n_drop

--- examples/configs/common.py
+++ examples/configs/common.py
@@ -28,8 +28,8 @@ MODEL_LOSS_NAME_DICT = {
 }
 
 # market setting for trading
-TOPK_LIST = [1, 2, 4, 6, 8, 10]
-N_DROP_LIST = [1, 2, 3, 4, 5]
+TOPK_LIST = [1, 2, 4, 6, 8, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+N_DROP_LIST = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25]
 
 BECHMARK_PARAMS = {
     "csi300": "SH000300",
